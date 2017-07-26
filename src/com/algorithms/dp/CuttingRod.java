33892f6ce530023218800ef09c9fcb89b342ff9b
Deborshi Saha <deborshi.saha@gmail.com>
2017-07-26 00:56:36 -0700

    Cutting Rod

--- src/com/algorithms/dp/CuttingRod.java
+++ src/com/algorithms/dp/CuttingRod.java
@@ -5,10 +5,30 @@
  */
 package com.algorithms.dp;
 
-/**
- *
- * @author dsaha
- */
 public class CuttingRod {
-    
+
+    public static void driver() {
+        int[] price = new int[]{1, 5, 8, 9, 10, 17, 17, 20};
+        CuttingRod obj = new CuttingRod();
+        System.out.println("Cutting Rod pricE:"+obj.max(price));
+    }
+
+    public int max(int[] price) {
+
+        int[] max = new int[price.length + 1];
+
+        // initialize the array with lets say no cuts
+        for (int i = 1; i <= price.length; i++) {
+            max[i] = price[i - 1];
+        }
+
+        // then we start chopping
+        for (int l = 1; l <= price.length; l++) {
+            for (int s = 1; s < l; s++) {
+                max[l] = Math.max(max[l], max[l - s] + max[s]);
+            }
+        }
+
+        return max[price.length];
+    }
 }
